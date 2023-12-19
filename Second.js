7d0883e77b61d13a829155a4230e846e6f0a0022
41047010s <107981898+41047010s@users.noreply.github.com>
2023-12-20 03:27:36 +0800

    update

--- Second.js
+++ Second.js
@@ -35,6 +35,38 @@ document.addEventListener('DOMContentLoaded', function () {
         option.addEventListener('click', function () {
             var selectedYear = this.textContent; // Retrieve the selected value
             console.log('Selected Year:', selectedYear);
+            d3.csv("vgsales.csv").then(data =>{
+                //console.log(data)
+                year=selectedYear;
+                //console.log(year);
+                preprocess(data);
+                let filteredData = filterYear(data, year);
+                //console.log(filteredData);
+                var newGenreCounts={};
+                var newGenrekeys;
+                var newPublisherCounts = {};
+                var newPublisherkeys;
+                var newPlatformCounts = {};
+                var newPlatformKeys;
+                processData(filteredData, newGenreCounts, newPublisherCounts, newPlatformCounts);
+                newGenrekeys = Object.keys(newGenreCounts);
+                newPublisherkeys = Object.keys(PublisherCounts);//console.log(Publisherkeys);
+                newPlatformKeys = Object.keys(PlatformCounts);
+                updateDonut(newGenreCounts, newGenrekeys);
+                var newSalesCounts=[0, 0, 0, 0];
+                processPie(filteredData, newSalesCounts)
+                updatePie(newSalesCounts);
+                const newtop5Publisher=[];
+                const newtop5Platform=[];
+
+                top5(newtop5Publisher, newPublisherCounts, newPublisherkeys);
+                updatebar(Platform_bar_rects, newtop5Platform, plat_bar_x, plat_bar_y)
+                
+
+
+
+                
+            })
         });
             
         dropdown.appendChild(option);
@@ -43,6 +75,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // Set maximum height and enable scrolling
     dropdown.style.maxHeight = maxDropdownHeight + 'px';
     dropdown.style.overflowY = 'auto';
+    
+    
 });
 
 var GenreCounts = {};
@@ -53,15 +87,27 @@ var Publisherkeys;
 var PlatformCounts = {};
 var PlatformKeys;
 var year; //1980-2016
-//var yearCounts={};
+var theDonut;
+    const radius=80;
+    const pie = d3.pie();
+    const arc = d3.arc()
+                .innerRadius(110)
+                .outerRadius(radius);
 
 const top5Publisher=[];
 const top5Platform=[];
 
+var thePie;
+    const pie_radius=100;
+    const pie_arc = d3.arc()
+                .innerRadius(0)
+                .outerRadius(pie_radius);
+
 d3.csv("vgsales.csv").then(data =>{
+
     preprocess(data);
-    processPie(data);//console.log(SalesCounts);
-    processData(data);//console.log(yearCounts);//console.log(GenreCounts)
+    processPie(data, SalesCounts);//console.log(SalesCounts);
+    processData(data, GenreCounts, PublisherCounts, PlatformCounts);//console.log(yearCounts);//console.log(GenreCounts)
     Genrekeys = Object.keys(GenreCounts);//console.log(keys);//console.log(PublisherCounts);
     Publisherkeys = Object.keys(PublisherCounts);//console.log(Publisherkeys);
     PlatformKeys = Object.keys(PlatformCounts);//console.log(PlatformKeys);
@@ -88,13 +134,22 @@ function preprocess(data) {
     //)
     //console.log(data)
 }
-
-function filterData()
+function filterYear(data, year)
 {
+    data.forEach(function (d) {
+        d.Year = Number(d.Year)
+        d.NA_Sales = Number(d.NA_Sales)
+        d.EU_Sales = Number(d.EU_Sales)
+        d.JP_Sales = Number(d.JP_Sales)
+        d.Other_Sales = Number(d.Other_Sales)
+        d.Global_Sales = Number(d.Global_Sales)
+    })
+    //console.log("inside", year)
+    return data.filter(d => d.Year == year);
 
 }
 
-function processData(data){
+function processData(data, GenreCounts, PublisherCounts, PlatformCounts){
     data.forEach(function(item){
         if(GenreCounts.hasOwnProperty(item.Genre))
         {
@@ -145,7 +200,7 @@ function processData(data){
 }
 
 
-function processPie(data)
+function processPie(data, SalesCounts)
 {
     data.forEach(function(d){
         SalesCounts[0]+=d.NA_Sales;
@@ -155,7 +210,7 @@ function processPie(data)
     })
 }
 
-function top5(top5array, data, keys)
+function top5(top5array, data)
 {
     top5array.length = 0; 
     
@@ -216,11 +271,7 @@ function sec_donut(data, keys)
             .text("Genre")
     
     //pie chart
-    const radius=80;
-    const pie = d3.pie();
-    const arc = d3.arc()
-                .innerRadius(110)
-                .outerRadius(radius);
+    
 
     var genre=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     genre.forEach(function(d, i){
@@ -228,7 +279,7 @@ function sec_donut(data, keys)
     })
                 
     const arcs = pie(genre);
-    var theDonut=group2.selectAll("path").data(arcs)
+    theDonut=group2.selectAll("path").data(arcs)
                     .enter().append("path").attr("d", arc)
                     .attr("fill", (d, i) => {
                         return donut_color[i];
@@ -236,9 +287,26 @@ function sec_donut(data, keys)
                     .attr("transform", 'translate('+(sec_donut_set.width/3+100)+', ' + (sec_donut_set.height+150) + ')');
 
 }
+function updateDonut(GenreCounts, keys)
+{
+    var genre=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    genre.forEach(function(d, i){
+        genre[i]=GenreCounts[keys[i]];
+    })
+    const arcs = pie(genre);
+    theDonut.data(arcs);
+    theDonut.transition()
+            .duration(1000)
+            .attrTween("d", function(d) {
+                var interpolate = d3.interpolate(this._current, d);
+                this._current = interpolate(0);
+                return function(t) {
+                    return arc(interpolate(t));
+                };
+    });
+}
 
-
-
+var pub_bar_rects;
 function sec_bar_pub(data)
 {
     const group2=svg2.append('g')
@@ -265,7 +333,7 @@ function sec_bar_pub(data)
     group2.append("g").call(bar_yAxisCall)
                 .attr("transform", 'translate('+sec_bar_set.width*99/100+', '+0+')')
 
-    var bar_rects=group2.append("g").selectAll("rect")
+    pub_bar_rects=group2.append("g").selectAll("rect")
                 .data(top5Publisher)
                 .enter().append("rect")
                 .attr("x", sec_bar_set.width*99/100)
@@ -275,11 +343,12 @@ function sec_bar_pub(data)
                 .style("fill", function (d, i) {
                     return bar_color[i];
                 })
-                //.attr("transfrom", 'translate(0, '+sec_bar_set.height+')')
 
 
 }
-
+var Platform_bar_rects
+var plat_bar_y;
+var plat_bar_x;
 function sec_bar_plat(data)
 {
     const group2=svg2.append('g').attr("transform", 'translate('+0+', '+(sec_bar_set.height)+')')
@@ -292,31 +361,37 @@ function sec_bar_plat(data)
                 .attr("font-size", "50px").attr("fill", "#004b62")
                 .text("Sales")
 
-    const bar_x = d3.scaleLinear()
+    plat_bar_x = d3.scaleLinear()
                 .domain([0, d3.max(top5Platform, d=>d.value)])
                 .range([0, sec_bar_set.width*2/3])
-    const bar_xAxisCall = d3.axisBottom(bar_x)
+    const bar_xAxisCall = d3.axisBottom(plat_bar_x)
     group2.append("g").call(bar_xAxisCall)
                 .attr("transform", 'translate('+sec_bar_set.width*99/100+', '+sec_bar_set.height*2/3+')')
     // Y label
-    const bar_y = d3.scaleBand()
+    plat_bar_y = d3.scaleBand()
                 .domain(top5Platform.map(d=>d.name))
                 .range([sec_bar_set.height*2/3, 0])
-    const bar_yAxisCall = d3.axisLeft(bar_y).ticks(10)
+    const bar_yAxisCall = d3.axisLeft(plat_bar_y).ticks(10)
     group2.append("g").call(bar_yAxisCall)
                 .attr("transform", 'translate('+sec_bar_set.width*99/100+', '+0+')')
 
-    var bar_rects=group2.append("g").selectAll("rect")
+    Platform_bar_rects=group2.append("g").selectAll("rect")
                 .data(top5Platform)
                 .enter().append("rect")
                 .attr("x", sec_bar_set.width*99/100)
-                .attr("y", d => bar_y(d.name))
-                .attr("width", d=> bar_x(d.value))
+                .attr("y", d => plat_bar_y(d.name))
+                .attr("width", d=> plat_bar_x(d.value))
                 .attr("height", 30)
                 .style("fill", function (d, i) {
                     return bar_color[i];
                 })
 
+}
+
+function updatebar(bar, top5array, bar_x, bar_y)
+{
+    //console.log("A")
+    bar.data(top5array).transition().duration(1000).attr("y", d => bar_y(d.name)).attr("width", d=> bar_x(d.value))
 }
 
 function sec_pie(data)
@@ -358,15 +433,11 @@ function sec_pie(data)
     
     //pie chart
     //const radius = Math.min(sec_pie_set.width, sec_pie_set.height) / 2;
-    const radius=100;
-    const pie = d3.pie();
-    const arc = d3.arc()
-                .innerRadius(0)
-                .outerRadius(radius);
+    
                 
-    const arcs = pie(SalesCounts);
-    var thePie=group2.selectAll("path").data(arcs)
-                    .enter().append("path").attr("d", arc)
+    const pie_arcs = pie(SalesCounts);
+    thePie=group2.selectAll("path").data(pie_arcs)
+                    .enter().append("path").attr("d", pie_arc)
                     .attr("fill", (d, i) => {
                         return pie_color[i];
                     })
@@ -375,4 +446,20 @@ function sec_pie(data)
     var tip = d3.tip().attr('class', 'd3-tip').html((d) => (pie_name[d.index]))
     group2.call(tip);
     thePie.on('mouseover', tip.show).on('mouseout', tip.hide);
+}
+
+function updatePie(SalesCounts)
+{
+    
+    const arcs = pie(SalesCounts);
+    thePie.data(arcs);
+    thePie.transition()
+            .duration(1000)
+            .attrTween("d", function(d) {
+                var interpolate = d3.interpolate(this._current, d);
+                this._current = interpolate(0);
+                return function(t) {
+                    return pie_arc(interpolate(t));
+                };
+    });
 }
